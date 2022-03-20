9f1b96c56e3a371d2baddb0707c12ac2c4eaa72e
June Stenzel <jstenzel@mit.edu>
2022-03-20 12:01:34 -0400

    Moved vs-build files to vs-build directory. Removed token_test_build.bat

--- vs-build/build.bat
+++ vs-build/build.bat
@@ -3,8 +3,12 @@ cd ..
 set COREFILES=engine\room.cpp engine\state.cpp engine\parser.cpp
 set SRCFILES=src\main.cpp src\token.cpp src\game_parser.cpp
 set INCLUDES="."
+set EXENAME=Underworld
 cls
 
-cmd /c "cl /I%INCLUDES% /EHsc %COREFILES% %SRCFILES% /link /out:Underworld.exe" 
+cmd /c "cl /I%INCLUDES% /EHsc %COREFILES% %SRCFILES% /link /out:%EXENAME%.exe" 
+
+move .\*.obj .\vs-build
+move .\%EXENAME%.exe .\vs-build
 
 cd vs-build
--- vs-build/token_test_build.bat
+++
@@ -1,9 +0,0 @@
-:: Run with the Developer Command Prompt for VS
-cls
-cd ..
-set FILES=src\token.cpp
-set INCLUDES="."
-set ARGS="/EHsc /std:c++20"
-
-cmd /c "cl /I%INCLUDES% %ARGS% %FILES% /link /out:TokenTest.exe" 
-cd vs-build

bb48e06ff87c7d3684ffedb281ae284056c4e368
June Stenzel <jstenzel@mit.edu>
2022-03-22 00:20:01 -0400

    Fixed weird tprint linker issues by moving templated functions to header

--- engine/utils.cpp
+++ engine/utils.cpp
@@ -106,28 +106,3 @@ void cprint_1(std::string name, std::string out)
 //the addToStream function helps us take arbitrary lengths of string input and concatenate them behind the scenes
 void addToStream(std::stringstream&)
 {}
-
-template<typename T, typename... Args>
-void addToStream(std::stringstream& a_stream, T&& a_value, Args&&... a_args)
-{
-    a_stream << std::forward<T>(a_value);
-    addToStream(a_stream, std::forward<Args>(a_args)...);
-}
-
-//formatted terminal printing of any number of strings
-template<typename... Args>
-void tprint(Args&&... a_args)
-{
-    std::stringstream s;
-    addToStream(s, std::forward<Args>(a_args)...);
-    tprint_1(s.str());
-}
-
-//nice conversation-formatted terminal printing of one string
-template<typename... Args>
-void cprint(std::string name, Args&&... a_args)
-{
-    std::stringstream s;
-    addToStream(s, std::forward<Args>(a_args)...);
-    cprint_1(name, s.str());
-}
--- engine/utils.h
+++ engine/utils.h
@@ -15,15 +15,29 @@ void cprint_1(std::string name, std::string out);
 void addToStream(std::stringstream&);
 
 template<typename T, typename... Args>
-void addToStream(std::stringstream& a_stream, T&& a_value, Args&&... a_args);
+void addToStream(std::stringstream& a_stream, T&& a_value, Args&&... a_args)
+{
+    a_stream << std::forward<T>(a_value);
+    addToStream(a_stream, std::forward<Args>(a_args)...);
+}
 
 //formatted terminal printing of any number of strings
 template<typename... Args>
-void tprint(Args&&... a_args);
+void tprint(Args&&... a_args)
+{
+    std::stringstream s;
+    addToStream(s, std::forward<Args>(a_args)...);
+    tprint_1(s.str());
+}
 
 //nice conversation-formatted terminal printing of one string
 template<typename... Args>
-void cprint(std::string name, Args&&... a_args);
+void cprint(std::string name, Args&&... a_args)
+{
+    std::stringstream s;
+    addToStream(s, std::forward<Args>(a_args)...);
+    cprint_1(name, s.str());
+}
 
 
 #endif
